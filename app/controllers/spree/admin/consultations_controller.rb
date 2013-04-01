856a511ee7118af3dafad58b910481d67c65b324
Lenart Rudel <lenart.rudel@gmail.com>
2013-04-01 23:48:15 +0100

    Load taxonomies when needed.

--- app/controllers/spree/admin/consultations_controller.rb
+++ app/controllers/spree/admin/consultations_controller.rb
@@ -1,8 +1,22 @@
 class Spree::Admin::ConsultationsController < Spree::Admin::ResourceController
 
+  before_filter :load_taxons, except: [:index, :destroy]
+
   def edit
-    @consultation ||= Spree::Consultation.find params[:id]
-    @taxons = Spree::Taxon.all
+    @consultation = Spree::Consultation.find params[:id]
+  end
+
+  def create
+    @consultation = Spree::Consultation.new params[:consultation]
+    @consultation.save
+
+    redirect_to edit_admin_consultation_path(@consultation), notice: "Consultation \"#{@consultation.name}\" created!"
+  end
+
+private
+
+  def load_taxons
+    @taxons ||= Spree::Taxon.all
   end
 
 end

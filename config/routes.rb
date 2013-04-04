1dc596b12fae0af9941133295910cc7ce3d655f5
Lenart Rudel <lenart.rudel@gmail.com>
2013-04-04 10:13:32 +0100

    Fix route for new classifications

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Spree::Core::Engine.routes.draw do
 
   namespace :admin do
     # Used for destroying records
-    resources :consultation_classifications, as: :classifications
+    resources :consultation_classifications, as: :classifications, only: :destroy
 
     resources :consultations do
       resources :consultation_classifications,
                 as: :classifications,
-                only: [:index, :create, :destroy]
+                only: [:index, :new, :create, :destroy]
     end
   end
 
